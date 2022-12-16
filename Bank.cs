1ed04085b1ab4d1cd1afa1392aab526ded93683d
EfratGrinboom <92879754+EfratGrinboom@users.noreply.github.com>
2022-12-16 08:36:34 +0200

    Add files via upload

---
+++ Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson7_SingletonDesignPattern
+{
+    sealed class Bank
+    {
+        double totalSum;
+        //1. private c-tor
+        //לא מאפשר הקצאת אובייקטים מחוץ למחלקה
+        //private Bank(){} 
+        Bank() { }
+
+        //2. משתנה סטטי פרטי, המכיל מופע של המחלקה
+        //static Bank instance;// = new Bank(); //כדי לא ליצור מופע אם לא צריך
+
+        //3. פונקציה סטטית, ציבורית, היוצרת את המופע היחיד- בפעם הראשונה
+        //ותמיד מחזירה את המופע היחיד שקיים
+        //public static Bank GetInstance()
+        //{
+        //    //if (instance == null)
+        //    //    instance = new Bank();
+        //    instance ??= new Bank();
+        //    return instance;
+        //}
+
+        //private static Bank instance;
+
+        static Bank()
+        {
+            Instance = new Bank();
+        }
+
+        public static Bank Instance { get; }
+       
+
+        public void Hafkada(double sum)
+        {
+            totalSum += sum;
+        }
+
+        //Singletonתוספת ל
+        //כדי לאפשר הקצאת מספר מסויים של מופעים
+        //שלב 1 יישאר
+        //ובמקום משתנה בודד מסוג המחלקה תהיה רשימה
+        //והטיפול ישתנה בהתאם
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson7_SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b1, b2;
+            //b1 = new Bank();
+
+            b1 = Bank.Instance;
+            b1.Hafkada(1000);
+
+            b2 = Bank.Instance;
+            b2.Hafkada(300);
+        }
+    }
+}
